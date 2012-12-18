cf0798847794ac2966efae08fd98f0d3d8324fb0
cmbasnett@gmail.com <cmbasnett@gmail.com@a047d7f6-1f86-c896-52c0-2ede5b36f5d6>
2012-12-18 11:42:23 +0000

    Fixed a potential crash.

--- trunk/ps2ls/Forms/ModelBrowser.Designer.cs
+++ trunk/ps2ls/Forms/ModelBrowser.Designer.cs
@@ -173,6 +173,9 @@
             this.glControl1.Size = new System.Drawing.Size(546, 600);
             this.glControl1.TabIndex = 1;
             this.glControl1.VSync = false;
+            this.glControl1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.glControl1_MouseDown);
+            this.glControl1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.glControl1_MouseMove);
+            this.glControl1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.glControl1_MouseUp);
             this.glControl1.Resize += new System.EventHandler(this.glControl1_Resize);
             // 
             // ModelBrowser
