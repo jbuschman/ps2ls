5a513bf671ed7d9967282bc0a826fdbfe4dcd727
cmbasnett@gmail.com <cmbasnett@gmail.com@a047d7f6-1f86-c896-52c0-2ede5b36f5d6>
2013-01-02 23:40:24 +0000

    Removed some redundant code from ArcBallCamera

---
+++ trunk/ps2ls/Cryptography/Jenkins.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ps2ls.Cryptography
+{
+    public static class Jenkins
+    {
+        //http://en.wikipedia.org/wiki/Jenkins_hash_function#one-at-a-time
+        public static UInt32 OneAtATime(String key)
+        {
+            UInt32 hash = 0;
+
+            for (Int32 i = 0; i < key.Length; ++i)
+            {
+                hash += key[i];
+                hash += (hash << 10);
+                hash ^= (hash >> 6);
+            }
+
+            hash += (hash << 3);
+            hash ^= (hash >> 11);
+            hash += (hash << 15);
+
+            return hash;
+        }
+    }
+}
