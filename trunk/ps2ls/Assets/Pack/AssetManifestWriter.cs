71419a0a9e5c59ea85743fc67c06caf90ccfa952
cmbasnett@gmail.com <cmbasnett@gmail.com@a047d7f6-1f86-c896-52c0-2ede5b36f5d6>
2013-07-05 17:00:45 +0000

    3 spooky 5 me

--- trunk/ps2ls/Assets/Pack/AssetManifestWriter.cs
+++ trunk/ps2ls/Assets/Pack/AssetManifestWriter.cs
@@ -8,19 +8,22 @@ using System.IO;
 using System.Threading;
 using System.ComponentModel;
 using System.IO.Compression;
+using System.Net;
+using System.Collections.Specialized;
 
 namespace ps2ls.Assets.Pack
 {
     public class AssetManifestWriter
     {
         private const int version = 1;
-        private GenericLoadingForm loadingForm = new GenericLoadingForm();
-        private BackgroundWorker writeBackgroundWorker = new BackgroundWorker();
+        private GenericLoadingForm loadingForm;
+        private BackgroundWorker writeBackgroundWorker;
 
         public AssetManifestWriter()
         {
-            writeBackgroundWorker.DoWork += writeDoWork;
+            writeBackgroundWorker = new BackgroundWorker();
             writeBackgroundWorker.WorkerReportsProgress = true;
+            writeBackgroundWorker.DoWork += writeDoWork;
             writeBackgroundWorker.ProgressChanged += writeProgressChanged;
             writeBackgroundWorker.RunWorkerCompleted += writeRunWorkerCompleted;
         }
@@ -43,14 +46,16 @@ namespace ps2ls.Assets.Pack
 
         public void Write(string path)
         {
-            writeBackgroundWorker.RunWorkerAsync(path);
-
+            loadingForm = new GenericLoadingForm();
             loadingForm.Show();
+
+            writeBackgroundWorker.RunWorkerAsync(path);
         }
 
         public void writeToPath(object sender, object arg)
         {
-            String path = (String)arg;
+            BackgroundWorker backgroundWorker = (BackgroundWorker)sender;
+            String path = System.IO.Path.GetTempFileName();
 
             int revision = 0;
 
@@ -59,39 +64,138 @@ namespace ps2ls.Assets.Pack
             GZipStream gzipStream = new GZipStream(fileStream, CompressionMode.Compress);
             BinaryWriter binaryWriter = new BinaryWriter(memoryStream);
             
-            char[] header = { 'P', 'S', '2', 'L', 'S', 'A', 'M' };
+            char[] header = { 'P', 'S', '2', 'L', 'S', 'A', 'M', 'F' };
             binaryWriter.Write(header);
             binaryWriter.Write(version);
             binaryWriter.Write(revision);
 
-            int assetCount = 0;
+            int checksum = 0;
 
+            //calculate manifest checksum from all files
             foreach (Pack pack in AssetManager.Instance.Packs)
-                assetCount += pack.Assets.Count;
+                foreach (Asset asset in pack.Assets)
+                    checksum += asset.Crc32;
+            
+            binaryWriter.Write(checksum);
 
-            binaryWriter.Write(assetCount);
+            int packCount = AssetManager.Instance.Packs.Count;
+
+            binaryWriter.Write(packCount);
 
             foreach (Pack pack in AssetManager.Instance.Packs)
             {
+                binaryWriter.Write((byte)pack.Name.Length);
+                binaryWriter.Write(ASCIIEncoding.ASCII.GetBytes(pack.Name));
+                binaryWriter.Write(pack.Checksum);
+
+                // write length of this pack block so we can skip ahead
+                // if the checksum is identical to whatever we have on
+                // record.
+                int length = getPackBlockLength(pack);
+                binaryWriter.Write(length);
+                binaryWriter.Write(pack.Assets.Count);
+
                 for (int i = 0; i < pack.Assets.Count; ++i)
                 {
                     Asset asset = pack.Assets[i];
 
-                    writeBackgroundWorker.ReportProgress((int)(((float)i / (float)assetCount) * 100.0f), asset.Name);
+                    //backgroundWorker.ReportProgress((int)(((float)i / (float)assetCount) * 100.0f), asset.Name);
 
-                    binaryWriter.Write(asset.Name);
-                    binaryWriter.Write(asset.Size);
-                    binaryWriter.Write(asset.AbsoluteOffset);
+                    binaryWriter.Write((byte)asset.Name.Length);
+                    binaryWriter.Write(ASCIIEncoding.ASCII.GetBytes(asset.Name));
                     binaryWriter.Write(asset.Crc32);
                 }
             }
 
-            writeBackgroundWorker.ReportProgress(100, "Compressing");
+            backgroundWorker.ReportProgress(100, "Compressing");
 
-            byte[] buffer = new byte[memoryStream.Length];
-            memoryStream.Read(buffer, 0, buffer.Length);
+            byte[] buffer = memoryStream.ToArray();
             gzipStream.Write(buffer, 0, buffer.Length);
             gzipStream.Close();
+
+            NameValueCollection nvc = new NameValueCollection();
+            nvc["whatever"] = "whatever";
+            HttpUploadFile(@"http://localhost/ps2ls/api/manifest_submit.php", path, "manifest", "application/octet-stream", null);
+        }
+
+        private int getPackBlockLength(Pack pack)
+        {
+            int length = 0;
+
+            foreach (Asset asset in pack.Assets)
+            {
+                length += 1 + asset.Name.Length;    //name
+                length += 4;                        //crc32
+            }
+
+            return length;
+        }
+
+        public static void HttpUploadFile(string url, string file, string paramName, string contentType, NameValueCollection nvc)
+        {
+            string boundary = "---------------------------" + DateTime.Now.Ticks.ToString("x");
+            byte[] boundarybytes = System.Text.Encoding.ASCII.GetBytes("\r\n--" + boundary + "\r\n");
+
+            HttpWebRequest wr = (HttpWebRequest)WebRequest.Create(url);
+            wr.ContentType = "multipart/form-data; boundary=" + boundary;
+            wr.Method = "POST";
+            wr.KeepAlive = true;
+            wr.Credentials = System.Net.CredentialCache.DefaultCredentials;
+
+            Stream rs = wr.GetRequestStream();
+
+            string formdataTemplate = "Content-Disposition: form-data; name=\"{0}\"\r\n\r\n{1}";
+            if (nvc != null)
+            {
+                foreach (string key in nvc.Keys)
+                {
+                    rs.Write(boundarybytes, 0, boundarybytes.Length);
+                    string formitem = string.Format(formdataTemplate, key, nvc[key]);
+                    byte[] formitembytes = System.Text.Encoding.UTF8.GetBytes(formitem);
+                    rs.Write(formitembytes, 0, formitembytes.Length);
+                }
+            }
+            rs.Write(boundarybytes, 0, boundarybytes.Length);
+
+            string headerTemplate = "Content-Disposition: form-data; name=\"{0}\"; filename=\"{1}\"\r\nContent-Type: {2}\r\n\r\n";
+            string header = string.Format(headerTemplate, paramName, file, contentType);
+            byte[] headerbytes = System.Text.Encoding.UTF8.GetBytes(header);
+            rs.Write(headerbytes, 0, headerbytes.Length);
+
+            FileStream fileStream = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.Read);
+            byte[] buffer = new byte[4096];
+            int bytesRead = 0;
+            while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) != 0)
+            {
+                rs.Write(buffer, 0, bytesRead);
+            }
+            fileStream.Close();
+
+            byte[] trailer = System.Text.Encoding.ASCII.GetBytes("\r\n--" + boundary + "--\r\n");
+            rs.Write(trailer, 0, trailer.Length);
+            rs.Close();
+
+            WebResponse wresp = null;
+            try
+            {
+                wresp = wr.GetResponse();
+                Stream stream2 = wresp.GetResponseStream();
+                StreamReader reader2 = new StreamReader(stream2);
+                string response = reader2.ReadToEnd();
+                Console.WriteLine(response);
+            }
+            catch (Exception)
+            {
+                if (wresp != null)
+                {
+                    wresp.Close();
+                    wresp = null;
+                }
+            }
+            finally
+            {
+                wr = null;
+            }
         }
     }
 }
---
+++ trunk/ps2ls/IO/LZHAM.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ps2ls.IO
+{
+    class LZHAM
+    {
+        public enum CompressLevel
+        {
+            Fastest = 0,
+            Faster,
+            Default,
+            Better,
+            Uber,
+            CompressLevels,
+            ForceDWORD = -1,
+        }
+
+        public enum CompressStatus
+        {
+            NotFinished = 0,
+            NeedsMoreInput,
+            FirstSuccessOrFailureCode,
+            Success = FirstSuccessOrFailureCode,
+            Failed,
+            FailedInitializing,
+            InvalidParameter,
+            OutputBufferTooSmall,
+            ForceDWORD = -1,
+        }
+
+        enum DecompressStatus
+        {
+            /// <summary>
+            /// LZHAM_DECOMP_STATUS_NOT_FINISHED indicates that the decompressor is flushing its output buffer (and there may be more bytes available to decompress).
+            /// </summary>
+            NotFinished = 0,
+            /// <summary>
+            /// LZHAM_DECOMP_STATUS_NEEDS_MORE_INPUT indicates that the decompressor has consumed all input bytes and has not encountered an "end of stream" code, so it's expecting more input to proceed.
+            /// </summary>
+            NeedsMoreInput,
+            /// <summary>
+            /// All the following enums always (and MUST) indicate failure/success.
+            /// </summary>
+            FirstSuccessOrFailureCode,
+            /// <summary>
+            /// LZHAM_DECOMP_STATUS_SUCCESS indicates decompression has successfully completed.
+            /// </summary>
+            Success = FirstSuccessOrFailureCode,
+            /// <summary>
+            /// The remaining status codes indicate a failure of some sort. Most failures are unrecoverable. TODO: Document which codes are recoverable.
+            /// </summary>
+            FirstFailureCode,
+            FailedInitializing = FirstFailureCode,
+            DestBufTooSmall,
+            HaveMoreOutput,
+            ExpectedMoreRawBytes,
+            BadCode,
+            Adler32,
+            BadRawBlock,
+            BadCompBlockSyncCheck,
+            InvalidParameter,
+        };
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct CompressParams
+        {
+            UInt32 StructSize;
+            UInt32 DictionarySizeLog2;
+            CompressLevel Level;
+            UInt32 MaxHelperThreads;
+            UInt32 CpuCacheTotalLines;
+            UInt32 CpuCacheLineSize;
+            UInt32 CompressFlags;
+            UInt32 NumSeedBytes;
+            IntPtr SeedBytes;
+        }
+
+        /// <summary>
+        /// Compresses an arbitrarily sized block of data, writing as much available compressed data as possible to the output buffer. 
+        /// This method may be called as many times as needed, but for best perf. try not to call it with tiny buffers.
+        /// pState - Pointer to internal compression state, created by lzham_compress_init.
+        /// pIn_buf, pIn_buf_size - Pointer to input data buffer, and pointer to a size_t containing the number of bytes available in this buffer. 
+        ///                         On return, *pIn_buf_size will be set to the number of bytes read from the buffer.
+        /// pOut_buf, pOut_buf_size - Pointer to the output data buffer, and a pointer to a size_t containing the max number of bytes that can be written to this buffer.
+        ///                         On return, *pOut_buf_size will be set to the number of bytes written to this buffer.
+        /// no_more_input_bytes_flag - Set to true to indicate that no more input bytes are available to compress (EOF). Once you call this function with this param set to true, it must stay set to true in all future calls.
+        ///
+        /// Normal return status codes:
+        ///    LZHAM_COMP_STATUS_NOT_FINISHED - Compression can continue, but the compressor needs more input, or it needs more room in the output buffer.
+        ///    LZHAM_COMP_STATUS_NEEDS_MORE_INPUT - Compression can contintue, but the compressor has no more output, and has no input but we're not at EOF. Supply more input to continue.
+        /// Success/failure return status codes:
+        ///    LZHAM_COMP_STATUS_SUCCESS - Compression has completed successfully.
+        ///    LZHAM_COMP_STATUS_FAILED, LZHAM_COMP_STATUS_FAILED_INITIALIZING, LZHAM_COMP_STATUS_INVALID_PARAMETER - Something went wrong.
+        /// </summary>
+        [DllImport(@"C:\Users\Colin\Desktop\lzham_x86.dll", EntryPoint = "lzham_compress", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public static extern CompressStatus Compress(IntPtr state, IntPtr inputBuffer, IntPtr inputBufferSize, IntPtr outputBuffer, IntPtr outputBufferSize, bool noMoreInputBytesFlag);
+        
+        /// <summary>
+        /// Deinitializes a decompressor.
+        /// returns adler32 of decompressed data if compute_adler32 was true, otherwise it returns the adler32 from the compressed stream.
+        /// </summary>
+        [DllImport(@"C:\Users\Colin\Desktop\lzham_x86.dll", EntryPoint = "lzham_decompress_deinit", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public static extern UInt32 DecompressDeinit(IntPtr pState);
+
+        /// <summary>
+        /// Initializes a compressor. Returns a pointer to the compressor's internal state, or NULL on failure.
+        /// pParams cannot be NULL. Be sure to initialize the pParams->m_struct_size member to sizeof(lzham_compress_params) (along with the other members to reasonable values) before calling this function.
+        /// TODO: With large dictionaries this function could take a while (due to memory allocation). I need to add a reinit() API for compression (decompression already has one).
+        /// </summary>
+        [DllImport(@"C:\Users\Colin\Desktop\lzham_x86.dll", EntryPoint = "lzham_compress_init", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr CompressInit(IntPtr compressParams);
+
+        /// <summary>
+        /// Single function call compression interface.
+        /// Same return codes as lzham_compress, except this function can also return LZHAM_COMP_STATUS_OUTPUT_BUF_TOO_SMALL.
+        /// </summary>
+        [DllImport(@"C:\Users\Colin\Desktop\lzham_x86.dll", EntryPoint = "lzham_compress_memory", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public static extern CompressStatus CompressMemory(IntPtr compressParams, IntPtr destinationBuffer, UInt64 destinationLength, IntPtr sourceBuffer, UInt64 sourceLength, IntPtr adler32);
+
+        //TODO: MORE FUNCTIONS
+
+        /// <summary>
+        /// Returns DLL version (LZHAM_DLL_VERSION).
+        /// </summary>
+        [DllImport(@"C:\Users\Colin\Desktop\lzham_x86.dll", EntryPoint = "lzham_get_version", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public static extern UInt32 GetVersion();
+    }
+}
