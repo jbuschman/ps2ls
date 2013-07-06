324f1e9d71efc33ed45867af6d512ef39f498f77
cmbasnett@gmail.com <cmbasnett@gmail.com@a047d7f6-1f86-c896-52c0-2ede5b36f5d6>
2013-07-06 01:10:11 +0000

    more lzham porting

---
+++ trunk/ps2ls/Forms/Controls/SearchTextTypeToolStripDrownDownButton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ps2ls.Forms.Controls
+{
+    public class SearchTextTypeToolStripDrownDownButton : ToolStripDropDownButton
+    {
+        public enum SearchTextType
+        {
+            Textual,
+            RegularExpression
+        }
+
+        SearchTextTypeToolStripDrownDownButton()
+        {
+            this.DropDownItems.Add("Textual", Properties.Resources.ui_label);
+            this.DropDownItems.Add("Regular Expression", Properties.Resources.regular_expression);
+        }
+    }
+}
--- trunk/ps2ls/Program.cs
+++ trunk/ps2ls/Program.cs
@@ -5,6 +5,7 @@ using ps2ls.Assets.Pack;
 using ps2ls.Forms;
 using ps2ls.Graphics.Materials;
 using Microsoft.Win32;
+using System.Runtime.InteropServices;
 
 namespace ps2ls
 {
@@ -16,9 +17,18 @@ namespace ps2ls
         [STAThread]
         static void Main()
         {
-            uint version = ps2ls.IO.LZHAM.GetVersion();
-            ps2ls.IO.LZHAM.CompressStatus compressStatus = ps2ls.IO.LZHAM.Compress(IntPtr.Zero, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero, false);
-            IntPtr asd = ps2ls.IO.LZHAM.CompressInit(IntPtr.Zero);
+            ps2ls.IO.LZHAM.DecompressParams decompressParams = new IO.LZHAM.DecompressParams();
+            decompressParams.ComputeAdler32 = 0;
+            decompressParams.DictSizeLog2 = ps2ls.IO.LZHAM.MinDictSizeLog2;
+            decompressParams.NumSeedBytes = 0;
+            decompressParams.OutputUnbuffered = 1;
+            decompressParams.SeedBytes = IntPtr.Zero;
+            decompressParams.StructSize = (uint)Marshal.SizeOf(decompressParams.GetType());
+
+            IntPtr decompressState = ps2ls.IO.LZHAM.DecompressInitialize(decompressParams);
+            Byte[] inputBuffer = new Byte[100];
+            Byte[] outputBuffer = new Byte[200];
+            ps2ls.IO.LZHAM.DecompressStatus decompressStatus = ps2ls.IO.LZHAM.Decompress(decompressState, inputBuffer, outputBuffer, true);
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
